e606931e97e5a56299b88445636e1a7bff1c7381
miibotree <miibotree@gmail.com>
2015-01-06 16:19:11 +0800

    AC code for PAT_1015

--- PAT_1015.c
+++ PAT_1015.c
@@ -6,20 +6,14 @@
 //		3.分别判断N和reverseN是不是素数
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <math.h>
 
-int Reverse(char *str, char *reverse_str, int str_len);
-int Cal_ReverseN(char *reverse_str, int str_len, int *reverse_n, int d);
+int Reverse(int n, int d);
 int Judge_Prime(int x);
 
 int main()
 {
 	int n = 0, d = 0, reverse_n = 0;//n的范围小于10^5
-	char str[256], reverse_str[256];
-	int str_len = 0;
-	int flag = 1;
 
 	while (scanf("%d", &n) != EOF)
 	{
@@ -27,21 +21,9 @@ int main()
 			break;
 		else
 			scanf("%d", &d);
-		memset(str, 0, 256);
-		memset(reverse_str, 0, 256);
-		flag = 1;
-		reverse_n = 0;
+		reverse_n = Reverse(n, d);
 
-		itoa(n, str, d);//将转换后的D进制数放在str字符串里面
-		str_len = strlen(str);
-		Reverse(str, reverse_str, str_len);
-		Cal_ReverseN(reverse_str, str_len, &reverse_n, d);//reverse_n中存放转换后的十进制
-		if (Judge_Prime(n) == false)
-			flag = 0;
-		if (Judge_Prime(reverse_n) == false)
-			flag = 0;
-
-		if (flag == 1)
+		if (Judge_Prime(n) && Judge_Prime(reverse_n))
 			printf("Yes\n");
 		else
 			printf("No\n");
@@ -49,36 +31,30 @@ int main()
 	return 0;
 }
 
-int Reverse(char *str, char *reverse_str, int str_len)
+int Reverse(int n, int d)
 {
-	int i = 0;
-	for (; i < str_len; i++)
-		reverse_str[str_len - i - 1] = str[i];
-	return 0;
-}
-
-int Cal_ReverseN(char *reverse_str, int str_len, int *reverse_n, int d)
-{
-	int i = str_len - 1;
-	int exp = 0;
-	double base = 0;
-	int coff = 0;
-
-
-	for (; i >= 0; i--)
+	int cnt = 0;
+	int b[100];
+	while (n)
 	{
-		coff = reverse_str[i] - '0';
-		base = d;
-		exp = str_len - i - 1;
-		*reverse_n += coff * pow(base, exp);
+		b[cnt] = n % d;
+		cnt++;
+		n = n / d;
 	}
-	return 0;
+	int ans = 0;
+	for (int i = 0; i < cnt; i++)
+		ans += b[cnt - i - 1] * pow((float)d,i);
+	return ans;
 }
 
 int Judge_Prime(int x)
 {
+	if (x < 2)
+		return false;
+
 	if (x == 2)
 		return true;
+
 	else
 	for (int i = 2; i < x / 2; i++)
 	{
