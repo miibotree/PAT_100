fbc4e8b1b273c3aa37d230ee31e8a89acced804c
miibotree <miibotree@gmail.com>
2015-01-15 21:40:02 +0800

    PAT_1020

--- PAT_1020.c
+++ PAT_1020.c
@@ -2,19 +2,23 @@
 //Miibotree
 //2015.1.15
 
-//Binary Tree Trace
-//Miibotree
-//2015.1.15
-//输入后序 中序 输出层次
-
 #include <stdio.h>
 #include <string.h>
+#include <queue>
 
-typedef struct Node{
+using namespace std;
+
+struct Node{
 	Node *lchild;
 	Node *rchild;
 	int value;
 }BT[31];
+
+queue<Node*> q;
+
+void LevelOrder(Node *BT, int n);
+Node *Create();
+Node* Build(int s1, int e1, int s2, int e2);
 
 int post_order[31];
 int in_order[31];
@@ -32,37 +36,54 @@ int main()
 		for (int i = 0; i < n; i++)
 			scanf("%d", &in_order[i]);
 		Node *T = Build(0, n-1, 0, n-1);
-		LevelOrder(BT, n);
+		LevelOrder(T, n);
 	}
 	return 0;
 }
 
 void LevelOrder(Node *BT, int n)
 {
-	for (int i = 0; i < n; i++)
+	int cnt = 0;
+	while(q.empty() == false)
+		q.pop();
+	Node* p;
+	q.push(BT);
+	while(q.empty() == false)
 	{
-		if (i == n - 1)
-			printf("%d\n", BT[i].value);
+		p = q.front();
+		q.pop();
+
+		if (cnt < n-1)
+		{
+			printf("%d ", p->value);
+			cnt++;
+		}
 		else
-			printf("%d ", BT[i].value);
+			printf("%d\n", p->value);
+
+		if (p->lchild != NULL)
+			q.push(p->lchild);
+		if (p->rchild != NULL)
+			q.push(p->rchild);
+
 	}
 }
 
 
 Node *Create()
 {
-	BT[pos].lchild == NULL;
-	BT[pos].rchild == NULL;
+	BT[pos].lchild = NULL;
+	BT[pos].rchild = NULL;
 	return &BT[pos++];
 }
 
 
 
-void Build(int s1, int e1, int s2, int e2)
+Node* Build(int s1, int e1, int s2, int e2)
 {
 	Node *ret = Create();
 	ret->value = post_order[e1];//后序遍历的最后一个节点肯定是根节点
-	int roorIdx;
+	int rootIdx;
 	for (int i = s2; i <= e2; i++)
 	{
 		if (in_order[i] == ret->value)
@@ -72,9 +93,9 @@ void Build(int s1, int e1, int s2, int e2)
 		}
 	}
 	if (rootIdx != s2)
-		Build(s1, s1+(rootIdx-s2)-1, s2, rootIdx-1);
+		ret->lchild = Build(s1, s1+(rootIdx-s2)-1, s2, rootIdx-1);
 	if (rootIdx != e2)
-		Build(s1+(rootIdx-s2), e1-1, roorIdx+1, e2);
+		ret->rchild = Build(s1+(rootIdx-s2), e1-1, rootIdx+1, e2);
 	return ret;
 }
 
